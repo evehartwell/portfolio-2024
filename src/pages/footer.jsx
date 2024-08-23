a649932146a3e681243b699dde897773a5811bd8
evehartwell <32525877+evehartwell@users.noreply.github.com>
2024-08-23 16:33:05 -0400

    update pdf

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -45,7 +45,7 @@ const Footer = () => {
                         className="footer-icon"
                     />
                 </ChakraLink>
-                <ChakraLink href={`${process.env.PUBLIC_URL}/resume-2024.pdf`} isExternal>
+                <ChakraLink href={`${process.env.PUBLIC_URL}/2024-Aug-Resume.pdf`} isExternal>
                     <IconButton
                         icon={<IoDocumentAttachOutline />}
                         aria-label="Resume"
