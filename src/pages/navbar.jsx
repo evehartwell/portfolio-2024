2ba4383ac25e02b90508463296cb512dbceba1bf
evehartwell <32525877+evehartwell@users.noreply.github.com>
2024-09-24 19:13:29 -0400

    fix nav responsiveness

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,24 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Flex, Button, Link as ChakraLink } from '@chakra-ui/react';
-import '../styles.css';
 
 const Navbar = () => {
     return (
-        <Flex className="navbar" align="center">
-            <Flex className="navbar-links">
-                <Link to="/" className="navbar-button">
-                    <Button className="navbar-button" variant="ghost">
+        <Flex
+            as="nav"
+            justify="center"
+            w="100%"
+            p="1rem"
+            position="fixed"
+            top="0"
+            left="0"
+            zIndex="1000"
+        >
+            <Flex
+                bg="rgba(214, 214, 214, 0.50)"
+                backdropFilter="blur(5px)"
+                borderRadius="30px"
+                gap={{ base: '30px', md: '80px' }}
+                border="1px solid"
+                borderColor="primary.1"
+            >
+                {/* Navbar buttons */}
+                <Link to="/">
+                    <Button
+                        variant="ghost"
+                        textTransform="uppercase"
+                        fontWeight="500"
+                        fontSize={{ base: 'sm', md: 'md' }}
+                        _hover={{ bg: 'none', color: 'primary.3' }}
+                    >
                         Work
                     </Button>
                 </Link>
-                <Link to="/about" className="navbar-button">
-                    <Button className="navbar-button" variant="ghost">
+                <Link to="/about">
+                    <Button
+                        variant="ghost"
+                        textTransform="uppercase"
+                        fontWeight="500"
+                        fontSize={{ base: 'sm', md: 'md' }}
+                        _hover={{ bg: 'none', color: 'primary.3' }}
+                    >
                         About
                     </Button>
                 </Link>
-                <ChakraLink href="mailto:evehartwell@gmail.com" isExternal className="navbar-button"> 
-                    <Button className="navbar-button" variant="ghost">
+                <ChakraLink href="mailto:evehartwell@gmail.com" isExternal> 
+                    <Button
+                        variant="ghost"
+                        textTransform="uppercase"
+                        fontWeight="500"
+                        fontSize={{ base: 'sm', md: 'md' }}
+                        _hover={{ bg: 'none', color: 'primary.3' }}
+                    >
                         Contact
                     </Button>
                 </ChakraLink>
